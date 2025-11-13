7bb9ab64b5c27f9d1aa109fbff847622627f0769
jonahmichael <jonah.paulin2596@gmail.com>
2025-11-13 15:55:47 +0530

    add: 10-Runnables notes

---
+++ 10-Runnables/notes.py
@@ -0,0 +1,9 @@
+# In 2022, when LLM models started to gain popularity,
+# there were many companies that offered API access to their models.
+# Some of the notable companies included OpenAI, Google, Microsoft, and Anthropic.
+
+# But each of these companies had different ways of structuring their API calls,
+# different authentication methods, and different response formats.
+
+# Therefore, they wanted build a framework that would allow them to easily switch between different LLM providers
+# without having to rewrite their entire codebase each time they wanted to use a different model. 
